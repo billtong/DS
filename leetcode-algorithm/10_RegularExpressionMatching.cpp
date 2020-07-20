728d625d24c63afebefa4de54098ff8c2148bfcd
billtong <zhiyuantongbill@gmail.com>
2020-07-20 00:15:44 -0400

    sol10 add dp method

--- leetcode-algorithm/10_RegularExpressionMatching.cpp
+++ leetcode-algorithm/10_RegularExpressionMatching.cpp
@@ -17,16 +17,45 @@ class Solution10
 public:
 
 	/*
+	Dynamic Programming Method (Bottom-Up)
+	dp(i, j): does text[i:] and pattern[j:] match
+	*/
+	bool isMatch(string s, string p)
+	{
+		int m = s.length(), n = p.length();
+		bool** t = new bool* [m + 1];
+		for (int i = 0; i < m + 1; i++)
+		{
+			t[i] = new bool[n + 1];
+			memset(t[i], false, n + 1);
+		}
+		t[m][n] = true;
+		for (int i = m; i >= 0; i--)
+		{
+			for (int j = n-1; j >= 0; j--) // here -1 becase the pattern needs 2 char to match each iteration
+			{
+				bool first_match = i < s.length() && (s.at(i) == p.at(j) || p.at(j) == '.');
+				if (j + 1 < p.length() && p.at(j + 1) == '*')
+					t[i][j] = t[i][j + 2] || (first_match && t[i + 1][j]);
+				else
+					t[i][j] = first_match && t[i + 1][j + 1];
+			}
+		}
+		return t[0][0];
+	}
+
+
+	/*
 	Recursive Method
 	Note: If a star is present in the pattern, it'll be in the second position pattern[1], then we may:
 		1. Ignore this part of the pattern
 		2. delete a matching character in the text
 	*/
-	bool isMatch(string s, string p)
+	bool isMatch_rec(string s, string p)
 	{
 		if (p.empty())
 			return s.empty();
-		bool first_match = !s.empty() && (s.at(0) == p.at(0) or p.at(0) == '.');
+		bool first_match = !s.empty() && (s.at(0) == p.at(0) || p.at(0) == '.');
 		int m = s.length(), n = p.length();
 		if (p.length() >= 2 && p.at(1) == '*') // Kleene star condition
 			return isMatch(s, p.substr(2, n - 2)) || (first_match && isMatch(s.substr(1, m - 1), p));
