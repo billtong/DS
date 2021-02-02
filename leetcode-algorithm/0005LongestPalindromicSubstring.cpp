4e0bd44a5f2cf5e64644f3cc670f9c90643e3b11
billtong <zhiyuantongbill@gmail.com>
2021-02-02 08:14:51 -0500

    trim solution 5 dynamic programming code

--- leetcode-algorithm/0005LongestPalindromicSubstring.cpp
+++ leetcode-algorithm/0005LongestPalindromicSubstring.cpp
@@ -24,11 +24,10 @@ public:
 
 	This solution here use bottom to top solution. (finish base cases initially)
 	*/
-	string longestPalindrome(string s)
+	string longestPalindrome1(string s)
 	{
-		if (s.empty())
-			return s;
-		int start = -1, max_len = 0;
+		if (s.empty()) return s;
+		int start = 0, max_len = 1;
 		int n = s.length();
 		bool** table = new bool* [n];
 		for (int i = 0; i < n; i++)
@@ -41,10 +40,9 @@ public:
 					table[i][j] = true;
 				else if (j - i == 1)  //p[i, i+1] = s[i] == s[i+1]
 					table[i][j] = s.at(i) == s.at(j);
-
 				else
 					table[i][j] = s.at(i) == s.at(j) && table[i + 1][j - 1]; //check if the s[j] s[i] same character, and then if substring s[i+1, j-1] palindromic
-				if (start < 0 || (table[i][j] && j - i + 1 > max_len))  //record the pos of palindrome substring
+				if (table[i][j] && j - i + 1 > max_len)  //record the pos of palindrome substring
 				{
 					start = i;
 					max_len = j - i + 1;
@@ -54,11 +52,37 @@ public:
 		return s.substr(start, max_len);
 	}
 
+	/* a cleaner version of the first function
+	using 2d vector, iterating in a more nature sequence.
+	combine if statements into condition operation
+	0,0
+	0,1 1,1
+	0,2 1,2 2,2
+	...
+	*/
+	string longestPalindrome2(string s) {
+		if (s.empty()) return s;
+		int n = s.length();
+		vector<vector<bool>> table(n, vector<bool>(n, false));
+		int start = 0, max_len = 1;
+		for (int j = 0; j < n; j++) {
+			for (int i = j; i >= 0; i--) {
+				table[i][j] = (i == j) || ((j - i == 1 || table[i + 1][j - 1]) && s.at(i) == s.at(j));
+				if (table[i][j] && max_len < j - i + 1) {
+					start = i;
+					max_len = j - i + 1;
+				}
+			}
+		}
+		return s.substr(start, max_len);
+	}
+
+
 	/*
 	check every char within string that whether they can form a palindrome
 	clear and easy one
 	*/
-	string longestPalindrome1(string s)
+	string longestPalindrome3(string s)
 	{
 		if (s.empty())
 			return s;
@@ -66,8 +90,8 @@ public:
 		int m = s.length();
 		for (int i = 0; i < m; i++)
 		{
-			int len1 = expandAroundCenter(s, i, i);		//search "cac" type
-			int len2 = expandAroundCenter(s, i, i + 1);	//search "caac" type
+			int len1 = expandAroundCenter(s, i, i);		//search "a" type
+			int len2 = expandAroundCenter(s, i, i + 1);	//search "aa" type
 			int len = len1 > len2 ? len1 : len2;
 			if (len > max_len)
 			{
